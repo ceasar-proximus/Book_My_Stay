1aee2bcff653428c5221e0dbedd41af3466d8ca4
ceasar-proximus <karan2006.p@gmail.com>
2026-04-08 09:11:27 +0530

    Finished UC1 code

---
+++ app/src/Book_My_Stay.java
@@ -0,0 +1,18 @@
+/**
+ * The entry point for the Hotel Booking Management System.
+ * This class demonstrates basic Java execution and console output.
+ *
+ * @author Karthik
+ * @version 1.0
+ */
+public class Book_My_Stay {
+    public static void main(String[] args) {
+
+        System.out.println("****************************************");
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("System: Hotel Booking Management");
+        System.out.println("Version: 1.0");
+        System.out.println("****************************************");
+
+    }
+}
